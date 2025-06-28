686cce087fd172285627cf18257d86228a50ff53
unknown <74901431+lemonjuiceexe@users.noreply.github.com>
2025-06-28 18:39:40 +0200

    Refactor drawVehicles function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { type Road, type SimulationStep } from "../server/types.ts";
+import { type Road, type SimulationStep } from "../shared/types.ts";
 import {
     calculateFirstVehiclePosition,
     drawBackground,
@@ -6,6 +6,7 @@ import {
     type Vector,
     vehicleRadius
 } from "./draw.ts";
+import { directionsAreEqual } from "../shared/helpers.ts";
 import "./style.css";
 
 startSimulation();
@@ -25,32 +26,47 @@ async function renderSimulation(data: SimulationStep[]): Promise<void> {
         const step: SimulationStep = data[i];
         setTimeout(() => {
             drawBackground(ctx);
-            let nextVehiclePosition: Record<Road, Vector> = calculateFirstVehiclePosition(canvas);
-            console.log(canvas.width, canvas.height);
-            console.log(nextVehiclePosition);
-            for (const vehicle of step.vehicles) {
-                const startRoad: Road = vehicle.direction.start;
-                const endRoad: Road = vehicle.direction.end;
-                const x: number = nextVehiclePosition[startRoad].x;
-                const y: number = nextVehiclePosition[startRoad].y;
-                drawVehicle(ctx, x, y);
-                switch (startRoad) {
-                    case "north":
-                        nextVehiclePosition[startRoad].y -= 2 * vehicleRadius + 10;
-                        break;
-                    case "south":
-                        nextVehiclePosition[startRoad].y += 2 * vehicleRadius + 10;
-                        break;
-                    case "east":
-                        nextVehiclePosition[startRoad].x += 2 * vehicleRadius + 10;
-                        break;
-                    case "west":
-                        nextVehiclePosition[startRoad].x -= 2 * vehicleRadius + 10;
-                        break;
-                }
-            }
+            drawStationaryVehicles(step, canvas);
         }, 2000 * i);
         // await new Promise((r) => setTimeout(r, 2000));
         // break;
+    }
+}
+function drawStationaryVehicles(step: SimulationStep, canvas: HTMLCanvasElement) {
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+    let nextVehiclePosition: Record<Road, Vector> = calculateFirstVehiclePosition(canvas);
+    let isFirstVehicle: Record<Road, boolean> = {
+        north: true,
+        south: true,
+        east: true,
+        west: true
+    };
+    for (const vehicle of step.vehicles) {
+        if (
+            !isFirstVehicle[vehicle.direction.start] ||
+            !step.greenDirections.some((greenDirection) =>
+                directionsAreEqual(greenDirection, vehicle.direction)
+            )
+        ) {
+            continue;
+        }
+        const startRoad: Road = vehicle.direction.start;
+        const x: number = nextVehiclePosition[startRoad].x;
+        const y: number = nextVehiclePosition[startRoad].y;
+        drawVehicle(ctx, x, y);
+        switch (startRoad) {
+            case "north":
+                nextVehiclePosition[startRoad].y -= 2 * vehicleRadius + 10;
+                break;
+            case "south":
+                nextVehiclePosition[startRoad].y += 2 * vehicleRadius + 10;
+                break;
+            case "east":
+                nextVehiclePosition[startRoad].x += 2 * vehicleRadius + 10;
+                break;
+            case "west":
+                nextVehiclePosition[startRoad].x -= 2 * vehicleRadius + 10;
+                break;
+        }
     }
 }
