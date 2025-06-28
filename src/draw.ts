bdd757bf72e308da236cb3eb68dfc42ed567f6cf
unknown <74901431+lemonjuiceexe@users.noreply.github.com>
2025-06-28 16:21:56 +0200

    Add drawing vehicles in starting positions

--- src/draw.ts
+++ src/draw.ts
@@ -1,13 +1,45 @@
-type CanvasVector = {
+import type { Road } from "../server/types.ts";
+
+export type Vector = {
     x: number;
     y: number;
 };
+
+const roadWidth: number = 60;
+const roadGap: number = 40;
+export const vehicleRadius: number = 15;
+
+export function calculateFirstVehiclePosition(canvas: HTMLCanvasElement): Record<Road, Vector> {
+    return {
+        north: {
+            x: canvas.width / 2 - roadGap / 2 - roadWidth / 2,
+            y: canvas.height / 2 - roadGap - roadWidth - 10
+        },
+        south: {
+            x: canvas.width / 2 + roadGap / 2 + roadWidth / 2,
+            y: canvas.height / 2 + roadGap + roadWidth + 10
+        },
+        east: {
+            x: canvas.width / 2 + roadGap + roadWidth + 10,
+            y: canvas.height / 2 - roadGap / 2 - roadWidth / 2
+        },
+        west: {
+            x: canvas.width / 2 - roadGap - roadWidth - 10,
+            y: canvas.height / 2 + roadGap / 2 + roadWidth / 2
+        }
+    };
+}
+export function drawVehicle(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    ctx.fillStyle = "#f00";
+    ctx.beginPath();
+    ctx.arc(x, y, vehicleRadius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
 export function drawBackground(ctx: CanvasRenderingContext2D): void {
     ctx.fillStyle = "#2b931f";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     // roads
-    const roadWidth: number = 80;
-    const roadGap: number = 40;
     ctx.fillStyle = "#555";
     ctx.fillRect(0, ctx.canvas.height / 2 - roadGap / 2 - roadWidth, ctx.canvas.width, roadWidth);
     ctx.fillRect(0, ctx.canvas.height / 2 + roadGap / 2, ctx.canvas.width, roadWidth);
@@ -19,53 +51,8 @@ export function drawBackground(ctx: CanvasRenderingContext2D): void {
         roadGap + roadWidth * 2,
         roadGap + roadWidth * 2
     );
-    // draw road markings
-    ctx.strokeStyle = "#fff";
-    ctx.lineWidth = 4;
-    ctx.setLineDash([20, 20]);
-    ctx.beginPath();
-    ctx.moveTo(0, ctx.canvas.height / 2 - roadGap / 2 - roadWidth / 2);
-    ctx.lineTo(
-        ctx.canvas.width / 2 - roadGap / 2 - roadWidth,
-        ctx.canvas.height / 2 - roadGap / 2 - roadWidth / 2
-    );
-    ctx.moveTo(ctx.canvas.width, ctx.canvas.height / 2 - roadGap / 2 - roadWidth / 2);
-    ctx.lineTo(
-        ctx.canvas.width / 2 + roadGap / 2 + roadWidth,
-        ctx.canvas.height / 2 - roadGap / 2 - roadWidth / 2
-    );
-    ctx.moveTo(0, ctx.canvas.height / 2 + roadGap / 2 + roadWidth / 2);
-    ctx.lineTo(
-        ctx.canvas.width / 2 - roadGap / 2 - roadWidth,
-        ctx.canvas.height / 2 + roadGap / 2 + roadWidth / 2
-    );
-    ctx.moveTo(ctx.canvas.width, ctx.canvas.height / 2 + roadGap / 2 + roadWidth / 2);
-    ctx.lineTo(
-        ctx.canvas.width / 2 + roadGap / 2 + roadWidth,
-        ctx.canvas.height / 2 + roadGap / 2 + roadWidth / 2
-    );
-    ctx.moveTo(ctx.canvas.width / 2 - roadGap / 2 - roadWidth / 2, 0);
-    ctx.lineTo(
-        ctx.canvas.width / 2 - roadGap / 2 - roadWidth / 2,
-        ctx.canvas.height / 2 - roadGap / 2 - roadWidth
-    );
-    ctx.moveTo(ctx.canvas.width / 2 - roadGap / 2 - roadWidth / 2, ctx.canvas.height);
-    ctx.lineTo(
-        ctx.canvas.width / 2 - roadGap / 2 - roadWidth / 2,
-        ctx.canvas.height / 2 + roadGap / 2 + roadWidth
-    );
-    ctx.moveTo(ctx.canvas.width / 2 + roadGap / 2 + roadWidth / 2, 0);
-    ctx.lineTo(
-        ctx.canvas.width / 2 + roadGap / 2 + roadWidth / 2,
-        ctx.canvas.height / 2 - roadGap / 2 - roadWidth
-    );
-    ctx.moveTo(ctx.canvas.width / 2 + roadGap / 2 + roadWidth / 2, ctx.canvas.height);
-    ctx.lineTo(
-        ctx.canvas.width / 2 + roadGap / 2 + roadWidth / 2,
-        ctx.canvas.height / 2 + roadGap / 2 + roadWidth
-    );
-    ctx.stroke();
     // stop lines
+    ctx.strokeStyle = "#fff";
     ctx.setLineDash([6, 3]);
     ctx.lineWidth = 6;
     ctx.beginPath();
@@ -103,7 +90,7 @@ export function drawBackground(ctx: CanvasRenderingContext2D): void {
     );
     ctx.stroke();
 
-    const trafficLightSize: CanvasVector = {
+    const trafficLightSize: Vector = {
         x: 60,
         y: 70
     };
@@ -111,43 +98,43 @@ export function drawBackground(ctx: CanvasRenderingContext2D): void {
     const centerY: number = ctx.canvas.height / 2;
     const offset: number = 80;
 
-    const trafficLightPositions: CanvasVector[] = [
+    const trafficLightPositions: Vector[] = [
         { x: centerX - offset - trafficLightSize.x, y: centerY - trafficLightSize.y / 2 },
         { x: centerX + offset, y: centerY - trafficLightSize.y / 2 },
         { x: centerX - trafficLightSize.x / 2, y: centerY - offset - trafficLightSize.y },
         { x: centerX - trafficLightSize.x / 2, y: centerY + offset }
     ];
 
-    trafficLightPositions.forEach((pos) => {
-        // Draw traffic light box
-        ctx.fillStyle = "#333";
-        ctx.fillRect(pos.x, pos.y, trafficLightSize.x, trafficLightSize.y);
-
-        ["red", "yellow"].forEach((color, index) => {
-            ctx.fillStyle = color;
-            ctx.beginPath();
-            ctx.arc(
-                pos.x + trafficLightSize.x / 2,
-                pos.y + ((index + 1) * trafficLightSize.y) / 4,
-                Math.min(trafficLightSize.x, trafficLightSize.y) / 8,
-                0,
-                Math.PI * 2
-            );
-            ctx.fill();
-        });
-        // Draw three green lights: left, forward, right
-        const greenY = pos.y + (3 * trafficLightSize.y) / 4;
-        const greenRadius = Math.min(trafficLightSize.x, trafficLightSize.y) / 8;
-        const greenOffsets = [
-            trafficLightSize.x / 4, // left
-            trafficLightSize.x / 2, // forward
-            (3 * trafficLightSize.x) / 4 // right
-        ];
-        greenOffsets.forEach((offsetX) => {
-            ctx.fillStyle = "green";
-            ctx.beginPath();
-            ctx.arc(pos.x + offsetX, greenY, greenRadius, 0, Math.PI * 2);
-            ctx.fill();
-        });
-    });
+    // trafficLightPositions.forEach((pos) => {
+    //     // Draw traffic light box
+    //     ctx.fillStyle = "#333";
+    //     ctx.fillRect(pos.x, pos.y, trafficLightSize.x, trafficLightSize.y);
+    //
+    //     ["red", "yellow"].forEach((color, index) => {
+    //         ctx.fillStyle = color;
+    //         ctx.beginPath();
+    //         ctx.arc(
+    //             pos.x + trafficLightSize.x / 2,
+    //             pos.y + ((index + 1) * trafficLightSize.y) / 4,
+    //             Math.min(trafficLightSize.x, trafficLightSize.y) / 8,
+    //             0,
+    //             Math.PI * 2
+    //         );
+    //         ctx.fill();
+    //     });
+    //     // Draw three green lights: left, forward, right
+    //     const greenY = pos.y + (3 * trafficLightSize.y) / 4;
+    //     const greenRadius = Math.min(trafficLightSize.x, trafficLightSize.y) / 8;
+    //     const greenOffsets = [
+    //         trafficLightSize.x / 4, // left
+    //         trafficLightSize.x / 2, // forward
+    //         (3 * trafficLightSize.x) / 4 // right
+    //     ];
+    //     greenOffsets.forEach((offsetX) => {
+    //         ctx.fillStyle = "green";
+    //         ctx.beginPath();
+    //         ctx.arc(pos.x + offsetX, greenY, greenRadius, 0, Math.PI * 2);
+    //         ctx.fill();
+    //     });
+    // });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,56 @@
-import { type OutputData } from "../server/types.ts";
-import { drawBackground } from "./draw.ts";
+import { type Road, type SimulationStep } from "../server/types.ts";
+import {
+    calculateFirstVehiclePosition,
+    drawBackground,
+    drawVehicle,
+    type Vector,
+    vehicleRadius
+} from "./draw.ts";
 import "./style.css";
 
 startSimulation();
 
 function startSimulation(): void {
     fetch("/output.json")
-        .then((res: Response): Promise<OutputData> => res.json())
-        .then((data: OutputData): void => {
+        .then((res: Response): Promise<SimulationStep[]> => res.json())
+        .then((data: SimulationStep[]): void => {
             renderSimulation(data);
         });
 }
-function renderSimulation(data: OutputData): void {
+async function renderSimulation(data: SimulationStep[]): Promise<void> {
+    console.log(data);
     const canvas: HTMLCanvasElement = document.querySelector("#canvas")!;
     const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
-    drawBackground(ctx);
+    for (let i: number = 0; i < data.length; i++) {
+        const step: SimulationStep = data[i];
+        setTimeout(() => {
+            drawBackground(ctx);
+            let nextVehiclePosition: Record<Road, Vector> = calculateFirstVehiclePosition(canvas);
+            console.log(canvas.width, canvas.height);
+            console.log(nextVehiclePosition);
+            for (const vehicle of step.vehicles) {
+                const startRoad: Road = vehicle.direction.start;
+                const endRoad: Road = vehicle.direction.end;
+                const x: number = nextVehiclePosition[startRoad].x;
+                const y: number = nextVehiclePosition[startRoad].y;
+                drawVehicle(ctx, x, y);
+                switch (startRoad) {
+                    case "north":
+                        nextVehiclePosition[startRoad].y -= 2 * vehicleRadius + 10;
+                        break;
+                    case "south":
+                        nextVehiclePosition[startRoad].y += 2 * vehicleRadius + 10;
+                        break;
+                    case "east":
+                        nextVehiclePosition[startRoad].x += 2 * vehicleRadius + 10;
+                        break;
+                    case "west":
+                        nextVehiclePosition[startRoad].x -= 2 * vehicleRadius + 10;
+                        break;
+                }
+            }
+        }, 2000 * i);
+        // await new Promise((r) => setTimeout(r, 2000));
+        // break;
+    }
 }
